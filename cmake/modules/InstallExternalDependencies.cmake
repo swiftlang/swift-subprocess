b445a056e700f0bc4b9a20ed2585a2c1bc3633c4
Evan Wilde <ewilde@apple.com>
2025-06-11 21:54:00 -0700

    Initial CMake Build
    Adding an initial CMake build for the Swift-subprocess library.
    At this time, it will build its own swift-system library, although we
    should allow it to search for one if one is available.
    
    CMake does not automatically generate modulemaps like SwiftPM does. As a
    result, we need to add one for Swift to import the C shims.
    
    There is one library target emitted from this build, Subprocess.

---
+++ cmake/modules/InstallExternalDependencies.cmake
@@ -0,0 +1,16 @@
+include_guard()
+
+# TODO: Use find_package to find a pre-built SwiftSystem
+
+include(FetchContent)
+
+FetchContent_Declare(SwiftSystem
+  GIT_REPOSITORY https://github.com/apple/swift-system.git
+  GIT_TAG a34201439c74b53f0fd71ef11741af7e7caf01e1 # 1.4.2
+  GIT_SHALLOW YES)
+list(APPEND dependencies SwiftSystem)
+
+
+if(dependencies)
+  FetchContent_MakeAvailable(${dependencies})
+endif()
