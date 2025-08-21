85e0124d4080c46989900ad1ca286e26cc79161d
Jake Petroules <jake.petroules@apple.com>
2025-08-20 21:37:40 -0700

    Fix the build when the SubprocessFoundation trait is disabled
    Closes #129

--- Tests/SubprocessTests/UnixTests.swift
+++ Tests/SubprocessTests/UnixTests.swift
@@ -487,6 +487,7 @@ internal func assertNewSessionCreated(
 
 // MARK: - Performance Tests
 extension SubprocessUnixTests {
+    #if SubprocessFoundation
     @Test(.requiresBash) func testConcurrentRun() async throws {
         // Launch as many processes as we can
         // Figure out the max open file limit
@@ -540,6 +541,7 @@ extension SubprocessUnixTests {
             try await group.waitForAll()
         }
     }
+    #endif
 }
 
 #endif  // canImport(Darwin) || canImport(Glibc)
