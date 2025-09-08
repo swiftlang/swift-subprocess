9208e412731e9e7f4a63eb120df5c52a5b481548
Chris McGee <christie_mcgee@apple.com>
2025-09-08 13:58:33 -0400

    Revert "Fix compile error"
    This reverts commit f8f226fe4bea1baf6017346ceb25590d399dd757.

--- Sources/Subprocess/IO/AsyncIO+Windows.swift
+++ Sources/Subprocess/IO/AsyncIO+Windows.swift
@@ -231,7 +231,7 @@ final class AsyncIO: @unchecked Sendable {
                         // * ERROR_INVALID_PARAMETER - The handle likely doesn't have FILE_FLAG_OVERLAPPED, which might indicate that it isn't a pipe
                         //   so we can just signal that it is ready for reading right away.
                         //
-                        continuation.yield(0)
+                        continuation.yield(UInt32.max)
                     }
                 }
                 // Now save the continuation
@@ -322,14 +322,17 @@ final class AsyncIO: @unchecked Sendable {
 
             }
 
-            // Depending on whether the handle is overlapped, or not find the bytes read
-            // from the pointer, or the signal stream.
             let bytesRead =
                 if bytesReadIfSynchronous == 0 {
                     try await signalStream.next() ?? 0
                 } else {
                     bytesReadIfSynchronous
                 }
+
+            // This handle doesn't support overlapped (asynchronous) I/O, so we must have read it synchronously above
+            if bytesRead == UInt32.max {
+                bytesRead = bytesReadIfSynchronous
+            }
 
             if bytesRead == 0 {
                 // We reached EOF. Return whatever's left
