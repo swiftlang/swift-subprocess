05d9966e780cb39e5bf75f26f54766069e88d9c0
Chris McGee <cmcgee1024@gmail.com>
2026-05-30 08:21:29 -0400

    Fix remaining license header check failure

--- scripts/prep-linux-swift.sh
+++ scripts/prep-linux-swift.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 ##===----------------------------------------------------------------------===##
 ##
 ## This source file is part of the Swift.org open source project
